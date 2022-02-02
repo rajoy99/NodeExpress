294d8baac0122d06851f880df237b29d4d23831d
rajoy99 <rafat.joy99@gmail.com>
2022-02-02 20:40:47 +0600

    express installed

---
+++ index.js
@@ -0,0 +1,64 @@
+const http = require('http')
+
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+let persons = [
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+app.post('/api/persons', (request, response) => {
+  const person = request.body
+  console.log(person)
+  console.log(typeof person)
+  response.json(person)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => {
+    console.log(person.id, typeof person.id, id, typeof id, person.id === id)
+    return person.id === id
+  })
+
+    
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(person => person.id !== id)
+
+  response.status(204).end()
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
